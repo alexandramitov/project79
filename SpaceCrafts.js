ffa1d3936a6b7d3f4481fff2d672204b99cc08b6
alexandramitov <71188919+alexandramitov@users.noreply.github.com>
2021-07-26 16:48:34 -0400

    Add files via upload

---
+++ SpaceCrafts.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default class SpaceCrafts extends React.Component{
+    render(){
+        return(
+            <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <Text> Space Crafts Screen </Text>
+            </View>
+        )
+    }
+}
